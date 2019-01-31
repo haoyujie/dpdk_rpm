983ab1fee0931ee9b431e11bead5b029258bab0c
CentOS Sources <bugs@centos.org>
2019-01-31 13:57:47 -0500

    import dpdk-18.11-2.el7_6

--- SOURCES/gen_config_group.sh
+++ SOURCES/gen_config_group.sh
@@ -70,6 +70,51 @@ do
     # Enable automatic driver loading from this path
     set_conf "${OUTDIR}" CONFIG_RTE_EAL_PMD_PATH '"/usr/lib64/dpdk-pmds"'
 
+    # Disable DPDK libraries not needed
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_TIMER n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_CFGFILE n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_JOBSTATS n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_LPM n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_ACL n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_POWER n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_SCHED n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_DISTRIBUTOR n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_REORDER n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PORT n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_TABLE n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PIPELINE n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_KNI n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_CRYPTODEV n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_SECURITY n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_FLOW_CLASSIFY n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_BBDEV n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_COMPRESSDEV n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_BPF n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_OCTEONTX_MEMPOOL n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_DPAA_MEMPOOL n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_DPAA2_MEMPOOL n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_CFGFILE n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_EFD n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_FLOW_CLASSIFY n
+
+    # Disable all eventdevs
+    for eventdev in $(grep _EVENTDEV= "${OUTDIR}/.config" | sed 's@=\(y\|n\)@@g')
+    do
+        set_conf "${OUTDIR}" $eventdev n
+    done
+
+    # Disable all rawdevs
+    for rawdev in $(grep _RAWDEV= "${OUTDIR}/.config" | sed 's@=\(y\|n\)@@g')
+    do
+        set_conf "${OUTDIR}" $rawdev n
+    done
+
+    # Disable virtio user
+    set_conf "${OUTDIR}" CONFIG_RTE_VIRTIO_USER n
+
+    # Enable vhost numa as libnuma dep is ok
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VHOST_NUMA y
+
     # start by disabling ALL PMDs
     for pmd in $(grep _PMD= "${OUTDIR}/.config" | sed 's@=\(y\|n\)@@g')
     do
@@ -81,29 +126,72 @@ do
     # as such
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_QAT n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_OCTEONTX_SSOVF n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_OCTEONTX_ZIPVF n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_VHOST n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_KNI n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_XENVIRT n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_NULL_CRYPTO n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_NULL n
-    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_TAP n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_CRYPTO_SCHEDULER n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_SKELETON_EVENTDEV n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_SW_EVENTDEV n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_PCAP n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_BOND n
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_AF_PACKET n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_SOFTNIC n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_DPAA2_SEC n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_DPAA_SEC n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_VIRTIO_CRYPTO n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_COMMON_DPAAX n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_CAAM_JR n
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_CAAM_JR_BE n
 
     # whitelist of enabled PMDs
     # Soft PMDs to enable
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_RING y
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_VHOST y
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VIRTIO_PMD y
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_TAP y
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PMD_FAILSAFE y
 
-    # HW PMDs
+
+    # start by disabling all buses
+    for bus in $(grep _BUS= "${OUTDIR}/.config" | sed 's@=\(y\|n\)@@g')
+    do
+        set_conf "${OUTDIR}" $bus n
+    done
+
+    # blacklist buses that don't conform to std naming
+    # May override VMBUS later in arch specific section
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VMBUS n
+
+    # whitelist buses
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_PCI_BUS y
+    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VDEV_BUS y
+
+
+    # Disable some other miscellanous items related to test apps
+    set_conf "${OUTDIR}" CONFIG_RTE_TEST_BBDEV n
+    set_conf "${OUTDIR}" CONFIG_RTE_APP_CRYPTO_PERF n
+
+    # Disable kernel modules
+    set_conf "${OUTDIR}" CONFIG_RTE_EAL_IGB_UIO n
+    set_conf "${OUTDIR}" CONFIG_RTE_KNI_KMOD n
+
+    # Disable experimental stuff
+    set_conf "${OUTDIR}" CONFIG_RTE_NEXT_ABI n
+
+    # Build DPDK as shared library
+    set_conf "${OUTDIR}" CONFIG_RTE_BUILD_SHARED_LIB y
+
+    # Compile the PMD test application
+    set_conf "${OUTDIR}" CONFIG_RTE_TEST_PMD y
+
+    # Arch specific
     set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_I40E_PMD y
     case "${DPDK_CONF_MACH_ARCH[i]}" in
     x86_64)
+        # Hw PMD
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_BNXT_PMD y
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_ENIC_PMD y
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_MLX4_PMD y
@@ -112,29 +200,17 @@ do
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_MLX5_DLOPEN_DEPS y
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_NFP_PMD y
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_QEDE_PMD y
+        # Sw PMD
+        set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_NETVSC_PMD y
+        set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VDEV_NETVSC_PMD y
+        # Bus
+        set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VMBUS y
         ;&
     arm64)
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_IXGBE_PMD y
         set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_IGB_PMD y
         ;;
     esac
-
-    # Compile the PMD test application
-    set_conf "${OUTDIR}" CONFIG_RTE_TEST_PMD y
-
-    # Enable vhost-numa build, the added deps are ok for us
-    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_VHOST_NUMA y
-
-    # Disable kernel modules
-    set_conf "${OUTDIR}" CONFIG_RTE_EAL_IGB_UIO n
-    set_conf "${OUTDIR}" CONFIG_RTE_LIBRTE_KNI n
-    set_conf "${OUTDIR}" CONFIG_RTE_KNI_KMOD n
-
-    # Disable experimental stuff
-    set_conf "${OUTDIR}" CONFIG_RTE_NEXT_ABI n
-
-    # Build DPDK as shared library
-    set_conf "${OUTDIR}" CONFIG_RTE_BUILD_SHARED_LIB y
 
     cp "${OUTDIR}/.config" "${OUTPUT_DIR}/${DPDK_TARGETS[$i]}-config"
 done
